752034945b2258b24d7d08b54de935c3d8f9cf22
viktortor <viktortor988@gmail.com>
2018-08-20 14:39:07 +0300

    lesson 3 commit

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -14,7 +14,7 @@ public class LinkedinLoginTest {
         //Enter userEmail
         //Enter userPassword
         //Click on 'Sign in' button
-        // Verify Home page is displayed
+        //Verify Home page is displayed
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
@@ -25,42 +25,62 @@ public class LinkedinLoginTest {
         Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up",
                 "Title is wrong.");
 
+
+
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
         WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
-        Assert.assertEquals(signInButton.getAttribute("id"), "login-submit",
-                "Sign In button is absent");
 
-
-        String loginEmail = "viktortor988@gmail.com";
-        String loginPassword = "Qwertyu-1";
-        WebElement loginEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        loginEmailField.click();
-        loginEmailField.clear();
-        loginEmailField.sendKeys(loginEmail);
-        //loginEmailField.sendKeys(TAB);
-
-        WebElement loginPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        loginPasswordField.click();
-        loginPasswordField.clear();
-        loginPasswordField.sendKeys(loginPassword);
-       // loginPasswordField.sendKeys(TAB);
-
+        Assert.assertTrue(signInButton.isDisplayed(),
+                "signIn button is noy displayed on Login page.");
+        userEmailField.sendKeys("linkedin.tsn.yanina@gmail.com");
+        userPasswordField.sendKeys("Yanina123");
         signInButton.click();
 
-       // Thread.sleep(5000);
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/",
+                "Home page URL is wrong.");
 
-        WebElement welcomeUserField = driver.findElement(By.xpath("//span[@class='Sans-17px-black-85%-semibold']"));
-        Assert.assertEquals(welcomeUserField.getText(), "Добро пожаловать, viktor!",
-                "no welcome message");
-
-        WebElement userProfileDropDown = driver.findElement(By.xpath("//button[@id='nav-settings__dropdown-trigger']"));
-        userProfileDropDown.click();
-
-        WebElement logoutButton = driver.findElement(By.xpath("//a[@data-control-name='nav.settings_signout']"));
-        Assert.assertEquals(logoutButton.getText(), "Выйти",
-                "LogOut button is absent");
+        Assert.assertEquals(driver.getTitle(),"LinkedIn",
+                "Home page Title is wrong.");
+        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id='profile-nav-item]"));
+        Assert.assertTrue(profileNavItem.isDisplayed(),
+                "profileNavItem button is not displayed on Home page.");
 
 
-       // driver.close();
+
+    }
+    @Test
+    public void negativLoginTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
+                "Login page URL is wrong.");
+
+        Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up",
+                "Title is wrong.");
+
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        Assert.assertTrue(signInButton.isDisplayed(),
+                "signIn button is noy displayed on Login page.");
+        userEmailField.sendKeys("a@b.c");
+        userPasswordField.sendKeys("Wrong");
+        signInButton.click();
+
+        sleep(3000);
+
+        //не забыть дома проверять тайтл и урл
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@role='alert']"));
+        Assert.assertEquals(alertMessage.getText(),
+                "There were one or more errors in your submission. Please correct the marked fields below.",
+                "Alert message text is wrong.");
+
+    }
+
+    private void sleep(int i) {
     }
 
 }
