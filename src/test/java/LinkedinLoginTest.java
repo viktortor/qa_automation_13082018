ea965d51313c704bfab334b6d70a5eb85984acc5
viktortor <viktortor988@gmail.com>
2018-08-27 14:29:43 +0300

    Lesson 4 commit

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,78 +3,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
 
 public class LinkedinLoginTest {
-    //Аннотация - сообщаем компилятору доп инфо - заставляем запускать наш метод
-    @Test (enabled = false)
-    public void successfullLoginTest () throws InterruptedException {
-        //Navigate to linkedin.com
-        //Verify that login page is loaded //наличие кнопки Войти
-        //Enter userEmail
-        //Enter userPassword
-        //Click on 'Sign in' button
-        //Verify Home page is displayed
-
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver; //виден всему классу
+    //Аннотация (@) - сообщаем компилятору доп инфо - заставляем запускать наш метод
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+    }
 
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
-         "Login page URL is wrong.");
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
 
-        Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up",
-                "Title is wrong.");
+    @Test //(enabled = false)
+    public void successfullLoginTest () throws InterruptedException {
 
+        LinkedInLoginPage linkedInLoginPage = new LinkedInLoginPage(driver); //объявили переменную linkedInLoginPage класса LinkedInLoginPage
+        Assert.assertTrue(linkedInLoginPage.isPageLoaded(),"Login page is not loaded");
+        linkedInLoginPage.login("getjman1@meta.ua", "Qwertyu-1");
 
-
-        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
-
-        Assert.assertTrue(signInButton.isDisplayed(),
-                "signIn button is noy displayed on Login page.");
-        userEmailField.sendKeys("getjman1@meta.ua");
-        userPasswordField.sendKeys("Qwertyu-1");
-        signInButton.click();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/",
                 "Home page URL is wrong.");
 
         Assert.assertEquals(driver.getTitle(),"LinkedIn",
                 "Home page Title is wrong.");
-        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id='profile-nav-item]"));
+
+        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
         Assert.assertTrue(profileNavItem.isDisplayed(),
                 "profileNavItem button is not displayed on Home page.");
-
-
-
     }
     @Test //(priority = 1)
     public void negativLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+        LinkedInLoginPage linkedInLoginPage = new LinkedInLoginPage(driver); //объявили переменную linkedInLoginPage класса LinkedInLoginPage
+        Assert.assertTrue(linkedInLoginPage.isPageLoaded(),"Login page is not loaded");
+        linkedInLoginPage.login("a@b.c", "Wrong");
 
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
-                "Login page URL is wrong.");
-
-        Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up",
-                "Title is wrong.");
-
-        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
-
-        Assert.assertTrue(signInButton.isDisplayed(),
-                "signIn button is noy displayed on Login page.");
-        userEmailField.sendKeys("a@b.c");
-        userPasswordField.sendKeys("Wrong");
-        signInButton.click();
-
-        sleep(3000);
-
-        //не забыть дома проверять тайтл и урл
+       //не забыть дома проверять тайтл и урл
         WebElement alertMessage = driver.findElement(By.xpath("//div[@role='alert']"));
         Assert.assertEquals(alertMessage.getText(),
                 "There were one or more errors in your submission. Please correct the marked fields below.",
@@ -82,17 +55,9 @@ public class LinkedinLoginTest {
 
     }
 
-    ///+некорректный формат мейла - Please enter a valid email address.
-    ///+неизвестный мейл Hmm, we don't recognize that email. Please try again.
-    ///+The password you provided must have at least 6 characters.
-    ///+Hmm, that's not the right password. Please try again or request a new one.
-    ///+The text you provided is too long (the maximum length is 128 characters, your text contains 238 characters).
-    ///+The password you provided must have at most 400 characters.
-
     @Test ///(priority = 1) //
     public void wrongEmailFormatWithoutDotLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -135,8 +100,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1) //
     public void wrongEmailFormatWithoutAtLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -180,8 +144,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1) //
     public void wrongEmailFormatAccountWithSpaceLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -224,8 +187,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1) //
     public void wrongEmailFormatDomainWithSpaceLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -268,8 +230,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1) //
     public void wrongEmailFormatWithoutAccountLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -312,8 +273,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1) //
     public void wrongEmailFormatWithoutDomainLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -356,8 +316,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1)
     public void wrongEmailLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -397,8 +356,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1)
     public void shortPasswordLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -438,8 +396,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1)
     public void wrongPasswordLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -479,8 +436,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1)
     public void longEmailLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -520,8 +476,7 @@ public class LinkedinLoginTest {
 
     @Test ///(priority = 1)
     public void longPasswordLoginTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/"); //полный формат адреса (с https....)
+
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Login page URL is wrong.");
@@ -558,5 +513,7 @@ public class LinkedinLoginTest {
                 "The password you provided must have at most 400 characters.",
                 "Alert message (passwordField) text is wrong.");
     }
+
+
 
 }
