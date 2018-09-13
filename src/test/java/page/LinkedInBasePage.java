571c0bcc0f15443f302d38d9d1657bbe35ea554a
viktortor <viktortor988@gmail.com>
2018-09-13 14:55:43 +0300

    Lesson 9 commit

--- src/test/java/page/LinkedInBasePage.java
+++ src/test/java/page/LinkedInBasePage.java
@@ -1,20 +1,29 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import util.GMailService;
 
 public class LinkedInBasePage {
     protected WebDriver driver;
 
-    public String getCurrentUrl(){
+    protected String getCurrentUrl(){
         return driver.getCurrentUrl();
     }
 
-    public String getCurrentTitle(){
+    protected String getCurrentTitle(){
         return driver.getTitle();
     }
 
     protected   static GMailService gMailService = new GMailService();
+
+    protected WebElement waitUntilElementVisible(WebElement webElement, int timeOutInSec){
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSec);
+        return wait.until(ExpectedConditions.visibilityOf(webElement));
+
+    }
 
 
 
